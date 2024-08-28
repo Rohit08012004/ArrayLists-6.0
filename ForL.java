020681b58f188d77fc260d6feb870f715bbc98b7
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-28 11:20:14 +0530

    done

--- ForL.java
+++ ForL.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ForL{
 public static void main(String[]args){
+
+  List<String> b=new ArrayList<String>();
+        
+        b.add("a");
+        b.add("c");
+        b.add("h");
+
+        b.add("g");
+        b.add("j");
+
+        for(String a:b){
+          System.out.println(a);
+        }
 // prepare an object for ArrayList
   // Load String instead of Integers 
   // Iterate them using for each loop
