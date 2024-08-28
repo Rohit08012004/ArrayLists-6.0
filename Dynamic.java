e3bfe1fea400987a23d45873209148bf59915114
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-28 11:04:46 +0530

    done

--- Dynamic.java
+++ Dynamic.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Dynamic{
 public static void main(String[]args){
 
 
-  List <Integer>k=new ArrayList<Integer>();
+  List <Person>k=new ArrayList<Person>();
   
-  k.add(3);
-  k.add(4);
-  k.add(5);
-  k.add(6);
-  k.add(7);
+  k.add(new Person(1, "Rohit", 3465));
+  k.add(new Person(1, "mohit", 321456));
+  k.add(new Person(1, "nanda", 786578426));
+  k.add(new Person(1, "surendra", 326745765));
 
+   Iterator<Person> l=k.iterator();
+   while (l.hasNext()) {
+    System.out.println(l.next());
+    
+   }
+  
 // Create an Object for ArrayList
   // Load person class Objects in it
   // Iterate it by using List Iterator
--- Person.java
+++ Person.java
@@ -1,3 +1,35 @@
 public class Person {
+    private int srno;
+    private String name;
+    private double salary;
+    public int getSrno() {
+        return srno;
+    }
+    public void setSrno(int srno) {
+        this.srno = srno; 
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public Person(int srno, String name, double salary) {
+        this.srno = srno;
+        this.name = name;
+        this.salary = salary;
+    }
+    public Person() {
+    }
+    @Override
+    public String toString() {
+        return "Person [srno=" + srno + ", name=" + name + ", salary=" + salary + "]";
+    }
     
 }
