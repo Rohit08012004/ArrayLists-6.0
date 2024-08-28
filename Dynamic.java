f20dfe4991e22d4d9776918e640c2e0c65c0cb9e
Rohit08012004 <rohitsthakre@gmail.com>
2024-08-28 10:51:50 +0530

    done

--- Dynamic.java
+++ Dynamic.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dynamic{
 public static void main(String[]args){
+
+
+  List <Integer>k=new ArrayList<Integer>();
+  
+  k.add(3);
+  k.add(4);
+  k.add(5);
+  k.add(6);
+  k.add(7);
+
 // Create an Object for ArrayList
   // Load person class Objects in it
   // Iterate it by using List Iterator
